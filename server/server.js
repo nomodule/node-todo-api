9939f028d848dfe422a33bf7372ad56b062c2935
Ajay <ajayr4217@gmail.com>
2018-09-03 23:49:55 +0530

    fixed finById error

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const bodyParser  = require('body-parser');
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/user');
 const { Todo } = require('./models/todo');
+const {ObjectID} = require('mongodb');
+
+// const id = 123;
+
+// if (!ObjectID(id)) {
+//     console.log('ID is not valid');
+// }
 
 const app = express();
 
@@ -29,6 +36,24 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
     });
 });
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((err) => {
+        res.send(400).send();
+    });
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
